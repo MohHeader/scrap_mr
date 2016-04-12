3aa2585ab04dd04907dee94658f88a563d8c977a
Mohammed Header <mohheader@hotmail.com>
2016-04-12 22:49:17 +0200

    Skip ~

--- amr/spiders/amr_spider.py
+++ amr/spiders/amr_spider.py
@@ -4,11 +4,12 @@ from amr.items import AmrItem
 class Amrpider(scrapy.Spider):
     name = "amr"
     allowed_domains = ["ldlp-dictionary.com"]
-    start_urls = ['http://www.ldlp-dictionary.com/dictionaries/word/%d/ /0/Al-Mughni Al-Akbar (En/Ar)' %(n) for n in range(4277183, 4277190)]
+    start_urls = ['http://www.ldlp-dictionary.com/dictionaries/word/%d/ /0/Al-Mughni Al-Akbar (En/Ar)' %(n) for n in range(4277183, 4277583)]
 # from  : 4277183
 # to    : 4391332
     def parse(self, response):
         item = AmrItem()
         item['english'] = response.css("#keywordContainer .keyword::text")[0].extract()
         item['arabic'] = response.css(".definitionListEntry::text")[0].extract()
-        yield item
+        if '~'not in item['english']:
+            yield item
